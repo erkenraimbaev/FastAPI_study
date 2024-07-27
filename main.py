3218909bd58cc4db0125868e3b9828ca05e529a0
erkenraimbaev <erken.orsk.2015@gmail.com>
2024-07-27 10:46:06 +0500

    Создан пакет users(префикс, тэг, валидация полей при создании юзера), подключен к main через роутер

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import uvicorn
-from fastapi import FastAPI, Path
-from pydantic import BaseModel, EmailStr
-from typing import Annotated
+from fastapi import FastAPI
+
+from users.views import router as users_router
 
 app = FastAPI()
-
-
-class CreateUser(BaseModel):
-    email: EmailStr
+app.include_router(users_router)
 
 
 @app.get('/')
@@ -19,18 +16,6 @@ def read_root():
 def read_name_and_print_hello(name: str):
     name = name.strip().title()
     return {'message': f'Hello, {name}'}
-
-
-@app.post('/users/create/')
-def create_user(user: CreateUser):
-    return {'message': 'User was created',
-            'email': user.email
-            }
-
-
-@app.get('/users/{user_id}')
-def get_user(user_id: Annotated[int, Path(ge=1, le=1000000)]):
-    return f'User has id: {user_id}'
 
 
 if __name__ == '__main__':
